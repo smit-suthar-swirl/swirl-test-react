3b8ea46781d91455a8876038711dc47d89b53cca
Smit Suthar <smit@goswirl.live>
2024-04-09 14:38:43 +0530

    added routers

--- src/JavascriptComp.js
+++ src/JavascriptComp.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SwirlShortVideos } from 'swirl-short-videos';
 import "./javascript.css"
+import { Link } from 'react-router-dom';
 const JavascriptComp = () => {
     const queryParams = Object.fromEntries(new URLSearchParams(window.location.search));
     return (
         <>
+            <Link to={`/typescript${window.location.search ? window.location.search : ""}`}>typescript</Link>
             <SwirlShortVideos dataCode={queryParams?.usercode ? queryParams?.usercode : "ifitdn8f"} dataPlalistCode={queryParams?.playlistcode ? queryParams?.playlistcode : "bD7Nkw"} />
         </>
     )
--- src/TypeScriptComp.js
+++ src/TypeScriptComp.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { SwirlShortVideos } from 'swirl-short-videos-typescript';
 
 const TypeScriptComp = () => {
     const queryParams = Object.fromEntries(new URLSearchParams(window.location.search));
+
     return (
         <>
+            <Link to={`/javascript${window.location.search ? window.location.search : ""}`}>javascript</Link>
             <SwirlShortVideos dataCode={queryParams?.usercode ? queryParams?.usercode : "ifitdn8f"} dataPlalistCode={queryParams?.playlistcode ? queryParams?.playlistcode : "bD7Nkw"} />
         </>
     )
