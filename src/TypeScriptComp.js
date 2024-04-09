db2cea92a27c40518b0bc773e0ea47f938af9a03
Smit Suthar <smit@goswirl.live>
2024-04-09 16:10:59 +0530

    video bg

--- src/TypeScriptComp.js
+++ src/TypeScriptComp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { SwirlShortVideos } from 'swirl-short-videos-typescript';
-
+import "./typescript.css"
 const TypeScriptComp = () => {
     const queryParams = Object.fromEntries(new URLSearchParams(window.location.search));
 
